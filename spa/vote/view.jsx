2899c664b95a313ca13b17e2ddbb60f18c0cd1b7
alet89 <alessandrotoschi@me.com>
2020-10-06 19:58:56 +0200

    rrr

--- spa/vote/view.jsx
+++ spa/vote/view.jsx
@@ -70,24 +70,23 @@ var Vote = React.createClass({
             <section className="LEADERSFINALAll">
                 {this.state && <section>
                     <section className="Status">
-                        {!this.state.started && <h3>
+                        <h5>Before to vote be sure to have read carefully the Proposal section</h5>
+                        {!this.state.started && <h2>
                             The survey will start at block <a target="_blank" href={window.getNetworkElement("etherscanURL") + "block/" + this.state.startBlock}>#{this.state.startBlock}</a>
-                        </h3>}
-                        {this.state.terminated && <h3>
+                        </h2>}
+                        {this.state.terminated && <h2>
                             The survey has been terminated at block <a target="_blank" href={window.getNetworkElement("etherscanURL") + "block/" + this.state.endBlock}>#{this.state.endBlock}</a>
-                        </h3>}
-                        {this.state.started && <section>
-                            <h3>Votes:</h3>
+                        </h2>}
+                        {this.state.running && <h2>
+                            End Block: <a target="_blank" href={window.getNetworkElement("etherscanURL") + "block/" + this.state.endBlock}>#{this.state.endBlock}</a>
+                        </h2>}
+                        {this.state.started && <section className="YOURVOTE">
+                            <h3>Status:</h3>
                             <h4>{window.fromDecimals(this.state.votes[0], 18)} Accept</h4>
                             <h4>{window.fromDecimals(this.state.votes[1], 18)} Refuse</h4>
-                            {window.walletAddress && <section>
-                                <h5>My Votes:</h5>
-                                <h6>{window.fromDecimals(this.state.myVotes.accepts.reduce((a, b) => parseInt(a) + parseInt(b)), 18)} Accept</h6>
-                                <h6>{window.fromDecimals(this.state.myVotes.refuses.reduce((a, b) => parseInt(a) + parseInt(b)), 18)} Refuse</h6>
-                            </section>}
                         </section>}
                     </section>
-                    {this.state.started && <section className="Actions">
+                    {this.state.started && <section className="Actions YOURVOTE">
                         {this.state.running && <section className="Input">
                             <a href="javascript:;" onClick={this.max}>Max</a>
                             <input type="text" ref={ref => this.input = ref} />
@@ -99,19 +98,28 @@ var Vote = React.createClass({
                             <span>Balance: {window.fromDecimals(this.state.balanceOf, this.state.selectedToken.decimals)} {this.state.selectedToken.symbol}</span>
                         </section>}
                         {this.state.running && window.walletAddress && <section className="Buttons">
-                            {this.state.performing !== 'approve' && <a className={"" + (this.state.approved ? " Disabled" : "")} href="javascript:;" data-action="approve" onClick={this.perform}>Approve {this.state.selectedToken.symbol}</a>}
+                            {this.state.performing !== 'approve' && <a className={"APPROVEBTN" + (this.state.approved ? " Disabled" : "")} href="javascript:;" data-action="approve" onClick={this.perform}>Approve {this.state.selectedToken.symbol}</a>}
                             {this.state.performing === 'approve' && <GhostLoader />}
-                            {this.state.performing !== 'accept' && <a className={"" + (!this.state.approved || this.state.performing ? " Disabled" : "")} href="javascript:;" data-action="accept" onClick={this.perform}>Accept</a>}
+                            {this.state.performing !== 'accept' && <a className={"VTNBTN" + (!this.state.approved || this.state.performing ? " Disabled" : "")} href="javascript:;" data-action="accept" onClick={this.perform}>Accept</a>}
                             {this.state.performing === 'accept' && <GhostLoader />}
-                            {this.state.performing !== 'refuse' && <a className={"" + (!this.state.approved || this.state.performing ? " Disabled" : "")} href="javascript:;" data-action="refuse" onClick={this.perform}>Refuse</a>}
+                            {this.state.performing !== 'refuse' && <a className={"VTNBTN" + (!this.state.approved || this.state.performing ? " Disabled" : "")} href="javascript:;" data-action="refuse" onClick={this.perform}>Refuse</a>}
                             {this.state.performing === 'refuse' && <GhostLoader />}
-                        </section>}
-                        {this.state.terminated && !this.state.redeemed && <section className="Buttons">
-                            {this.state.performing !== 'redeem' && <a className={"" + (this.state.performing ? " Disabled" : "")} href="javascript:;" data-action="redeem" onClick={this.perform}>Redeem</a>}
-                            {this.state.performing === 'redeem' && <GhostLoader />}
                         </section>}
                         {!window.walletAddress && <section className="Buttons">
                             <a className="" href="javascript:;" onClick={() => window.ethereum.enable().then(this.controller.loadData)}>Connect</a>
+                        </section>}
+                    </section>}
+                    {window.walletAddress && <section className="YOURVOTE">
+                        <h5>Your Votes:</h5>
+                        <h6>{window.fromDecimals(this.state.myVotes.accepts.reduce((a, b) => parseInt(a) + parseInt(b)), 18)} Accept</h6>
+                        <h6>{window.fromDecimals(this.state.myVotes.refuses.reduce((a, b) => parseInt(a) + parseInt(b)), 18)} Refuse</h6>
+                        <p>10 BUIDL</p>
+                        <p>100 UniFi</p>
+                        <p>10000 ARTE</p>
+                        <p>You'll be able to redeem your tokens here at <a target="_blank" href={window.getNetworkElement("etherscanURL") + "block/" + this.state.endBlock}>the end of the Proposal</a></p>
+                        {this.state.terminated && !this.state.redeemed && <section className="Buttons">
+                            {this.state.performing !== 'redeem' && <a className={"" + (this.state.performing ? " Disabled" : "")} href="javascript:;" data-action="redeem" onClick={this.perform}>Redeem</a>}
+                            {this.state.performing === 'redeem' && <GhostLoader />}
                         </section>}
                     </section>}
                 </section>}
